c51f9888b43c584d8268f2af2218bde3cb675d5e
DavyCraft648 <67502532+DavyCraft648@users.noreply.github.com>
2024-03-12 02:46:07 +0700

    Fix broken ClientboundPlayerInfoUpdatePacket serializer

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/PlayerListPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/PlayerListPacket.java
@@ -2,8 +2,10 @@ package org.barrelmc.barrel.network.translator.bedrock;
 
 import com.github.steveice10.mc.auth.data.GameProfile;
 import com.github.steveice10.mc.protocol.data.game.PlayerListEntry;
+import com.github.steveice10.mc.protocol.data.game.PlayerListEntryAction;
 import com.github.steveice10.mc.protocol.data.game.entity.player.GameMode;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundPlayerInfoRemovePacket;
+import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundPlayerInfoUpdatePacket;
 import net.kyori.adventure.text.Component;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
@@ -11,6 +13,7 @@ import org.barrelmc.barrel.utils.Utils;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.UUID;
 
 public class PlayerListPacket implements BedrockPacketTranslator {
@@ -28,7 +31,7 @@ public class PlayerListPacket implements BedrockPacketTranslator {
                 }
 
                 PlayerListEntry[] playerListEntriesL = playerListEntries.toArray(new PlayerListEntry[0]);
-                //player.getJavaSession().send(new ClientboundPlayerInfoUpdatePacket(EnumSet.of(PlayerListEntryAction.ADD_PLAYER, PlayerListEntryAction.INITIALIZE_CHAT, PlayerListEntryAction.UPDATE_LISTED, PlayerListEntryAction.UPDATE_LATENCY, PlayerListEntryAction.UPDATE_GAME_MODE, PlayerListEntryAction.UPDATE_DISPLAY_NAME), playerListEntriesL));
+                player.getJavaSession().send(new ClientboundPlayerInfoUpdatePacket(EnumSet.of(PlayerListEntryAction.ADD_PLAYER, PlayerListEntryAction.INITIALIZE_CHAT, PlayerListEntryAction.UPDATE_LISTED, PlayerListEntryAction.UPDATE_LATENCY, PlayerListEntryAction.UPDATE_GAME_MODE, PlayerListEntryAction.UPDATE_DISPLAY_NAME), playerListEntriesL));
                 break;
             }
             case REMOVE: {
